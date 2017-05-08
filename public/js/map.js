68e24deb3b3d477d75b150e80e0c14d59e6a10f3
ehacinom <ehacinom@gmail.com>
2017-05-08 00:13:36 -0500

    rewrite to black minimalist

--- public/js/map.js
+++ public/js/map.js
@@ -1,17 +1,24 @@
 function initialize_gmaps() {
+    
     // initialize new google maps LatLng object
     var myLatlng = new google.maps.LatLng(41.8885,-87.6354);
+    
     // set the map options hash
     var mapOptions = {
         center: myLatlng,
-        zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        styles: dark
+        zoom: 13,
+        mapTypeControl: false,
+        styles: dark,
+        backgroundColor: 'dark grey',
+        streetViewControl: false,
+        scaleControl: false,
+        zoomControl: false
     };
     // get the maps div's HTML obj
     var map_canvas_obj = document.getElementById("map-canvas");
     // initialize a new Google Map with the options
     var map = new google.maps.Map(map_canvas_obj, mapOptions);
+    
     // Add the marker to the map
     var marker = new google.maps.Marker({
         position: myLatlng,
@@ -21,6 +28,6 @@ function initialize_gmaps() {
     marker.setMap(map);
 }
 
-  $(document).ready(function() {
-      initialize_gmaps();
-  });
+$(document).ready(function() {
+    initialize_gmaps();
+});
